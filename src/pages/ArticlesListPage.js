631a2482d3750ff92ddaffe902d1abd29d8f9680
Ganeshan Ruwan Kumara <ruwan.ganeshan@acentura.com>
2024-01-04 19:51:00 +0530

    article mapping

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,8 +1,16 @@
+import articles from "./article-conent";
+import { Link } from "react-router-dom";
 const ArticlesListPage = () => {
     return(
-        <h1>
-            this is the articles list page!
-        </h1>
+        <>
+        <h1> articles </h1>
+        {articles.map(article => (
+            <Link to={'/articles/${article.name}'}>
+                <h3>{article.title}</h3>
+                <p>{article.content[0].substring(0,200)}....</p>
+            </Link>
+        ) )}
+                </>
     )
 }
 
